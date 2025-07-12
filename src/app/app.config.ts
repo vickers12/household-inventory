7e48efc1ee894a3a7b45b2981edb3c181b44d9da
Vicky Comeau <vickycomeau@hotmail.capnpm>
2025-07-11 20:19:47 -0400

    Initial commit and installed AnularFire

---
+++ src/app/app.config.ts
@@ -0,0 +1,24 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'inventory-app-fe5ba',
+    appId: '1:1098446114722:web:a0d6c156e9a592b0cc2cfb',
+    storageBucket: 'inventory-app-fe5ba.firebasestorage.app',
+    apiKey: 'AIzaSyDEWE5BjVTWQzELNYcbQvlP5d_qOsx7FCU',
+    authDomain: 'inventory-app-fe5ba.firebaseapp.com',
+    messagingSenderId: '1098446114722',
+    measurementId: 'G-D781CLPMCW',
+  },
+};
